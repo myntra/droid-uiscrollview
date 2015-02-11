9f73c17c0086d13260f07bb16446192057fd3850
Pawan <pawan.kumar1@myntra.com>
2015-02-11 19:20:52 +0530

    simple demo module

--- app/src/main/java/com/greycellofp/demo/MainActivity.java
+++ app/src/main/java/com/greycellofp/demo/MainActivity.java
@@ -1,10 +1,9 @@
-package com.greycellofp.droiduiscrollview;
+package com.greycellofp.demo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
